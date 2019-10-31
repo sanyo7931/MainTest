8f8307d4544fe9b869cb61d04e557e082f63aa05
wupojung <wupojung@gmail.com>
2019-10-31 18:56:35 +0800

    加入專案檔案。

---
+++ CalculatorDemo/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDemo
+{
+    public class Calculator
+    {
+        public int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public int Sub(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+        public int Mul(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+        public float Div(int n1, int n2)
+        {
+            return (float)n1 / (float)n2;
+        }
+    }
+}
---
+++ CalculatorDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            Console.WriteLine($"3+5={calc.Add(3, 5)}");
+            Console.WriteLine($"3-5={calc.Sub(3, 5)}");
+            Console.WriteLine($"3*5={calc.Mul(3, 5)}");
+            Console.WriteLine($"3/5={calc.Div(3, 5)}");
+            Console.ReadKey();
+        }
+    }
+}
